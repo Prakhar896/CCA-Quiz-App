80e41fb22afda38af3d28edfdade1fd3ca5d3cad
Prakhar Trivedi <prakhar0706@gmail.com>
2019-09-03 15:29:15 +0800

    Initial Commit

---
+++ CCA Quiz AppTests/CCA_Quiz_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CCA_Quiz_AppTests.swift
+//  CCA Quiz AppTests
+//
+//  Created by ssoeuser on 3/9/19.
+//  Copyright © 2019 ssoeuser. All rights reserved.
+//
+
+import XCTest
+@testable import CCA_Quiz_App
+
+class CCA_Quiz_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
